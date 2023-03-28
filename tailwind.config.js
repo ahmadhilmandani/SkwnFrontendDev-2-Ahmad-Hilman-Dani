e15e1efe677d7b65e8d3dd2a501b74caf86d73f6
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2023-03-29 05:17:34 +0700

    UPDATE all set for production process

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'brown-color': '#2F241F',
+        'light-brown-color': '#817253',
+        'greenish-color': '#E5F0B6',
+        'darkish-blue-color': '#667085',
+        'light-grey-color': '#FCFAFA',
+        'white-color': '#fff',
+      }
+    },
+  },
+  plugins: [],
+}
+
