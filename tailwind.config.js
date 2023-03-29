fdfe3b522c5869f05a2c25fd6853f1e1ac9e3aff
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2023-03-29 07:26:21 +0700

    UPDATE index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
       colors: {
         'blue-color': '#2F80FE',
         'light-blue-color': '#3BCFE9',
+        'very-light-blue-color': '#E4EAFF',
         'red-color': '#D12053',
         'light-red-color': '#FCE8ED',
       }
